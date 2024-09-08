6b1e0ba12b00738c479928a66e320f4e99a9594d
Rahul-Das-Sarma <rahuldas35748@gmail.com>
2024-09-08 21:15:06 +0530

    chore: add binary tree

--- Trees/BinarySearchTrees.js
+++ Trees/BinarySearchTrees.js
@@ -10,4 +10,49 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val === current.val) return undefined;
+      if (val < current.val) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  find(val) {
+    let current = this.root;
+    if (!this.root) return undefined;
+    if (val < current.val) {
+    }
+  }
 }
+
+const bst = new BinarySearchTree();
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(2);
+bst.insert(6);
+bst.insert(80);
+bst.insert(72);
+bst.insert(94);
+bst.insert(61);
+
+console.log(bst);
