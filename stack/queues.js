110bd41342f14bc863dcb0dda3c974ab49a857c8
Rahul-Das-Sarma <rahuldas35748@gmail.com>
2024-09-08 13:31:35 +0530

    chore: stacks and queues

---
+++ stack/queues.js
@@ -0,0 +1,47 @@
+// FIFO method
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+const queue = new Queue();
+
+queue.enqueue("GG");
+queue.enqueue("BG");
+queue.enqueue("BGG");
+
+console.log(queue);
