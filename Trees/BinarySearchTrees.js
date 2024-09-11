c7bdc18d5abe2dc3259a9f2971f6febcf2684f49
Rahul-Das-Sarma <rahuldas35748@gmail.com>
2024-09-11 19:52:15 +0530

    chore: add bfs

--- Trees/BinarySearchTrees.js
+++ Trees/BinarySearchTrees.js
@@ -52,17 +52,49 @@ class BinarySearchTree {
     if (!found) return false;
     return current;
   }
+
+  contains(val) {
+    if (!this.root) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (val < current.val) {
+        current = current.left;
+      } else if (val > current.val) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+      if (!found) return false;
+      return true;
+    }
+  }
+  // Tree Traversal
+  BFS() {
+    const visited = [];
+    const queue = [];
+    let node = this.root;
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      visited.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return visited;
+  }
 }
 
 const bst = new BinarySearchTree();
 
 bst.insert(10);
-bst.insert(5);
-bst.insert(2);
 bst.insert(6);
-bst.insert(80);
-bst.insert(72);
+bst.insert(3);
+bst.insert(8);
+bst.insert(15);
+bst.insert(20);
 bst.insert(94);
 bst.insert(61);
 
-console.log(bst.find(10));
+console.log(bst.find(8));
