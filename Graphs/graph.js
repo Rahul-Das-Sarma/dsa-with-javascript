5d4851ba47ad866d904a51469e5fa00bed2995fe
Rahul-Das-Sarma <rahuldas35748@gmail.com>
2024-09-25 10:47:00 +0530

    chore: dijkstras algorithm

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -40,18 +40,89 @@ class Graph {
     // // Finally, delete the vertex from the adjacency list
     // delete this.adjacentcyList[vertex];
   }
+
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacentcyList = this.adjacentcyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacentcyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) return dfs(neighbor);
+      });
+    })(start);
+    return result;
+  }
+
+  depthFirstIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacentcyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+
+  breadthFirst(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+    visited[start] = true;
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacentcyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 const graph = new Graph();
 
-graph.addVertex("Tokyo");
-graph.addVertex("Dallas");
-graph.addVertex("San Francisco");
-graph.addEdge("Tokyo", "Dallas");
-graph.addEdge("Tokyo", "San Francisco");
-graph.addEdge("Tokyo", "Jumanji");
-graph.addEdge("Dallas", "Jumanji");
-graph.addEdge("San Francisco", "Jumanji");
-// graph.removeEdge("Tokyo", "Jumanji");
-graph.removeVertex("Jumanji");
-console.log(graph.adjacentcyList);
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("A", "B");
+graph.addEdge("C", "E");
+graph.addEdge("D", "E");
+graph.addEdge("D", "F");
+graph.addEdge("E", "F");
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
+
+console.log(graph.breadthFirst("A"));
